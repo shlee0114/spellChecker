36c37acade5ae3182f2417211b0cbc38a5e66906
lsh <tlgj255@naver.com>
2021-10-11 21:26:54 +0900

    Feat : parameter로 text값 전달 및 get -> post로 변경

--- src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarCheckController.kt
+++ src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarCheckController.kt
@@ -1,21 +1,19 @@
 package com.grammer.grammerchecker.grammar_checker
 
 import com.fasterxml.jackson.databind.JsonNode
-import org.springframework.web.bind.annotation.GetMapping
 
 import com.grammer.grammerchecker.utils.ApiUtils.Companion.ApiResult
 import com.grammer.grammerchecker.utils.ApiUtils.Companion.success
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
 @RequestMapping("api")
 class GrammarCheckController(val service: GrammarCheckService) {
 
-    @GetMapping("check")
-    fun checkSpell() : ApiResult<JsonNode>{
-        val result = service.checkGrammar("되요")
+    @PostMapping("check")
+    fun checkAllSpell(@RequestParam("text") text : String) : ApiResult<JsonNode>{
+        val result = service.checkGrammar(text)
 
         return success(result)
     }
