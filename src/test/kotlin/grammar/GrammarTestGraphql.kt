bb2c643d05548a7f9a0d3f995db839a6e7673e3a
lsh <tlgj255@naver.com>
2021-10-13 22:57:33 +0900

    Add : graphQL 테스트 코드 작성

--- src/test/kotlin/grammar/GrammarTestGraphql.kt
+++ src/test/kotlin/grammar/GrammarTestGraphql.kt
@@ -1,13 +1,22 @@
 package grammar
 
 import com.grammer.grammerchecker.GrammerCheckerApplication
+import org.apache.commons.lang3.RandomStringUtils
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matchers
+import org.json.JSONException
+import org.json.JSONObject
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
 
 @SpringBootTest(classes = [GrammerCheckerApplication::class])
 @AutoConfigureMockMvc
@@ -26,10 +35,42 @@ class GrammarTestGraphql {
     @Order(1)
     @DisplayName("검색 성공")
     fun checkSuccess(){
-        mockMvc.perform(
-            MockMvcRequestBuilders.post("/graphql")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"query\":\"{check(text : \\\"되요\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"))
+
+        val query = "{\"query\":\"{check(text : \\\"되요\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"
+
+        val mvcResult = mockMvc.perform(post("/graphql")
+            .content(query)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(request().asyncStarted())
+            .andExpect(request().asyncResult(CoreMatchers.notNullValue()))
+            .andReturn()
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.data.check").isNotEmpty)
+            .andExpect(jsonPath("$.errors").doesNotExist())
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("검색 성공(수정 없음)")
+    fun checkSuccessReturnIsNull(){
+
+        val query = "{\"query\":\"{check(text : \\\"돼요\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"
+
+        val mvcResult = mockMvc.perform(post("/graphql")
+            .content(query)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(request().asyncStarted())
+            .andExpect(request().asyncResult(CoreMatchers.notNullValue()))
+            .andReturn()
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.data.check.errorText").isEmpty)
+            .andExpect(jsonPath("$.errors").doesNotExist())
     }
 
     @Test
@@ -37,6 +78,19 @@ class GrammarTestGraphql {
     @DisplayName("검색 실패(최대 길이 500자 초과)")
     fun checkFailedBecauseMaxLengthExceeded(){
 
+        val query = "{\"query\":\"{check(text : \\\"${RandomStringUtils.randomAlphanumeric(501)}\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"
+
+        val mvcResult = mockMvc.perform(post("/graphql")
+            .content(query)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(request().asyncStarted())
+            .andExpect(request().asyncResult(CoreMatchers.notNullValue()))
+            .andReturn()
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+            .andDo(print())
+            .andExpect(jsonPath("$.data.check").isEmpty)
+            .andExpect(jsonPath("$.errors").exists())
     }
 
     @Test
@@ -44,6 +98,19 @@ class GrammarTestGraphql {
     @DisplayName("검색 실패(빈 텍스트 전달)")
     fun checkFailedBecauseEmptyText(){
 
+        val query = "{\"query\":\"{check(text : \\\"\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"
+
+        val mvcResult = mockMvc.perform(post("/graphql")
+            .content(query)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(request().asyncStarted())
+            .andExpect(request().asyncResult(CoreMatchers.notNullValue()))
+            .andReturn()
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+            .andDo(print())
+            .andExpect(jsonPath("$.data.check").isEmpty)
+            .andExpect(jsonPath("$.errors").exists())
     }
 
     @Test
@@ -51,5 +118,19 @@ class GrammarTestGraphql {
     @DisplayName("검색 실패(띄어쓰기 발견)")
     fun checkFailedBecauseWhiteSpace(){
 
+        val query = "{\"query\":\"{check(text : \\\"이거 돼요\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"
+
+        val mvcResult = mockMvc.perform(post("/graphql")
+            .content(query)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(request().asyncStarted())
+            .andExpect(request().asyncResult(CoreMatchers.notNullValue()))
+            .andReturn()
+
+        mockMvc.perform(asyncDispatch(mvcResult))
+            .andDo(print())
+            .andExpect(jsonPath("$.data.check").isEmpty)
+            .andExpect(jsonPath("$.errors").exists())
     }
+
 }
