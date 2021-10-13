5261ae05d35b7485b2cbfd3f0b3af35bec07a0c0
lsh <tlgj255@naver.com>
2021-10-13 12:53:27 +0900

    Add : GraphQL Test 추가

--- src/test/kotlin/grammar/GrammarTestGraphql.kt
+++ src/test/kotlin/grammar/GrammarTestGraphql.kt
@@ -1,13 +1,13 @@
 package grammar
 
 import com.grammer.grammerchecker.GrammerCheckerApplication
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 
 @SpringBootTest(classes = [GrammerCheckerApplication::class])
 @AutoConfigureMockMvc
@@ -20,5 +20,36 @@ class GrammarTestGraphql {
     @Autowired
     fun setMockMvc(mockMvc: MockMvc){
         this.mockMvc = mockMvc
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("검색 성공")
+    fun checkSuccess(){
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/graphql")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"query\":\"{check(text : \\\"되요\\\") { errorText fixedText }}\",\"variables\":null,\"operationName\":null}"))
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("검색 실패(최대 길이 500자 초과)")
+    fun checkFailedBecauseMaxLengthExceeded(){
+
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("검색 실패(빈 텍스트 전달)")
+    fun checkFailedBecauseEmptyText(){
+
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("검색 실패(띄어쓰기 발견)")
+    fun checkFailedBecauseWhiteSpace(){
+
     }
 }
