4ed521df9b2e16fb8e80f99239d3aa8608faecf8
lsh <tlgj255@naver.com>
2021-10-14 12:24:52 +0900

    Delete : 사용하지 않는 파일 제거

--- frontend/grammar-check/src/index.js
+++ frontend/grammar-check/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './Root';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Root/>
+  <App/>
 ,document.getElementById('root'));
