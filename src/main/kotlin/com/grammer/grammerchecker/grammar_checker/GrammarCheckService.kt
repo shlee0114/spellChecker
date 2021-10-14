6fc396ab4c66f0a63013546b6e90dab0b106ab2c
lsh <tlgj255@naver.com>
2021-10-15 08:20:52 +0900

    Style : yml 정리 및 네이버 관련 요소들 property로 관리

--- src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarCheckService.kt
+++ src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarCheckService.kt
@@ -2,6 +2,7 @@ package com.grammer.grammerchecker.grammar_checker
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import graphql.com.google.common.base.Optional
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
@@ -11,6 +12,16 @@ import org.springframework.web.client.RestTemplate
 
 @Service
 class GrammarCheckService {
+
+    @Value("\${naver.url}")
+    private lateinit var naverUrl : String
+
+    @Value("\${naver.user-gent}")
+    private lateinit var naverUserGent : String
+
+    @Value("\${naver.referer}")
+    private lateinit var naverReferer : String
+
     @Transactional(readOnly = true)
     fun checkGrammar(grammar : String) : Optional<Array<GrammarDto>>{
 
@@ -21,13 +32,13 @@ class GrammarCheckService {
         }
 
         val header = HttpHeaders()
-        header.set("user-gent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36                                         (KHTML, like Gecko)     Chrome/57.0.2987.133 Safari/537.36")
-        header.set("referer", "https://search.naver.com/")
+        header.set("user-gent", naverUserGent)
+        header.set("referer", naverReferer)
 
         val entity = HttpEntity<String>("", header)
 
         val restTemplate = RestTemplate()
-        val respEntity = restTemplate.exchange("https://m.search.naver.com/p/csearch/ocontent/spellchecker.nhn?_callback=window.__jindo2_callback._spellingCheck_0&q=$grammar", HttpMethod.GET, entity, String::class.java)
+        val respEntity = restTemplate.exchange("$naverUrl$grammar", HttpMethod.GET, entity, String::class.java)
 
         val objectMapper = ObjectMapper()
 
@@ -39,9 +50,11 @@ class GrammarCheckService {
 
         val jsonNode = objectMapper.readTree(jsonString)
 
-        val errorCount = jsonNode.get("message").get("result").get("errata_count").toString().toInt()
-        val originFullText = jsonNode.get("message").get("result").get("origin_html").toString()
-        val fixedFullText = jsonNode.get("message").get("result").get("html").toString()
+        val result = jsonNode.get("message").get("result")
+
+        val errorCount = result.get("errata_count").toString().toInt()
+        val originFullText = result.get("origin_html").toString()
+        val fixedFullText = result.get("html").toString()
 
         val originTextArray = originFullText.split("</span>")
         val fixedTextArray = fixedFullText.split("</span>")
