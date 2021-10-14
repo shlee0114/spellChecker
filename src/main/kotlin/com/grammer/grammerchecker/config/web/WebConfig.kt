8742e02dc2905feb8c9d937bb7702032f1ef0d40
lsh <tlgj255@naver.com>
2021-10-14 23:22:12 +0900

    Add : cors 추가

---
+++ src/main/kotlin/com/grammer/grammerchecker/config/web/WebConfig.kt
@@ -0,0 +1,15 @@
+package com.grammer.grammerchecker.config.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer{
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+    }
+}
