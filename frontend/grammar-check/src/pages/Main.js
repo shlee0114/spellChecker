8e002c5d79b67a2d41798f8eb936b4762d11967f
lsh <tlgj255@naver.com>
2021-10-14 12:36:45 +0900

    Add : 메인 디자인 추가

--- frontend/grammar-check/src/pages/Main.js
+++ frontend/grammar-check/src/pages/Main.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import '../main.css'
 
 export default function Main (){
 
+    const inputText = (e) => {
+
+    }
 
     return (
         <article>
+            <section className="checkGrammar">
+                <label className="totalTextCount">0/500</label>
+                <label className="infoLabel">ctrl + `키를 누르시면 맞춤법 수정이 됩니다.</label>
+                <button className="checkAll">전체 검사</button>
+                <div className="textarea">
+                    <textarea className="inputArea" onChange={()=>inputText(this)}>
+
+                    </textarea>
+                    <textarea className="resultArea">
+
+                    </textarea>
+                </div>
+            </section>
+            <section className="searchHistory">
+                <button className="checkWord">변경 낱말 검사</button>
+                <button className="checkSentence">변경 문장 검사</button>
+            </section>
         </article>
     )
 }
