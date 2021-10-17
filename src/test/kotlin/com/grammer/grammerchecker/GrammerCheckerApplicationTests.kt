0367de0ae492a0e9dc1c7d85e95f823bbe53b868
lsh <tlgj255@naver.com>
2021-10-17 22:37:54 +0900

    Fix : 테스트 케이스 에러 수정

--- src/test/kotlin/com/grammer/grammerchecker/GrammerCheckerApplicationTests.kt
+++ src/test/kotlin/com/grammer/grammerchecker/GrammerCheckerApplicationTests.kt
@@ -3,7 +3,7 @@ package com.grammer.grammerchecker
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [GrammerCheckerApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GrammerCheckerApplicationTests {
 
     @Test
