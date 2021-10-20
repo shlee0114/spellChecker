e2a835ad32d08f59599304c8b59b5a4567200e00
lsh <tlgj255@naver.com>
2021-10-20 22:05:05 +0900

    Add : grammar router 설정

---
+++ src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarRouter.kt
@@ -0,0 +1,23 @@
+package com.grammer.grammerchecker.grammar_checker
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.coRouter
+
+
+@Configuration(proxyBeanMethods = false)
+class GrammarRouter(private val handler: GrammarHandler) {
+
+    @Bean
+    fun route() = coRouter {
+        "/api".nest{
+            accept(MediaType.APPLICATION_JSON).nest{
+                GET("/check", handler::checkAllSpell)
+                GET("/log", handler::logList)
+                POST("/log", handler::insertLog)
+            }
+        }
+    }
+
+}
