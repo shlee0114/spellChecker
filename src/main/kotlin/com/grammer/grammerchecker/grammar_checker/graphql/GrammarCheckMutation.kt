3b1252bf8c80cb71530e65d64121e80b5d2d3a28
lsh <tlgj255@naver.com>
2021-10-21 23:20:51 +0900

    Fix : graphQL 컴파일 에러만 제거

--- src/main/kotlin/com/grammer/grammerchecker/grammar_checker/graphql/GrammarCheckMutation.kt
+++ src/main/kotlin/com/grammer/grammerchecker/grammar_checker/graphql/GrammarCheckMutation.kt
@@ -1,17 +1,15 @@
 package com.grammer.grammerchecker.grammar_checker.graphql
 
+import com.expediagroup.graphql.spring.operations.Mutation
 import com.grammer.grammerchecker.grammar_checker.GrammarCheckService
 import com.grammer.grammerchecker.grammar_checker.LogRequest
 import graphql.GraphQLException
-import graphql.kickstart.tools.GraphQLMutationResolver
 import org.springframework.stereotype.Component
-import javax.transaction.Transactional
 import javax.validation.Valid
 
 @Component
-class GrammarCheckMutation(val service: GrammarCheckService) : GraphQLMutationResolver {
+class GrammarCheckMutation(val service: GrammarCheckService) : Mutation {
 
-    @Transactional
     fun log(@Valid log: LogRequest) : Boolean{
         try {
             service.insertWordLog(log)
--- src/main/kotlin/com/grammer/grammerchecker/grammar_checker/graphql/GrammarCheckResolver.kt
+++ src/main/kotlin/com/grammer/grammerchecker/grammar_checker/graphql/GrammarCheckResolver.kt
@@ -1,34 +1,30 @@
 package com.grammer.grammerchecker.grammar_checker.graphql
 
+import com.expediagroup.graphql.spring.operations.Query
 import com.grammer.grammerchecker.grammar_checker.GrammarCheckService
 import com.grammer.grammerchecker.grammar_checker.GrammarDto
 import com.grammer.grammerchecker.grammar_checker.GrammarRequest
 import com.grammer.grammerchecker.grammar_checker.LogDto
 import graphql.GraphQLException
-import graphql.kickstart.tools.GraphQLQueryResolver
 import org.springframework.stereotype.Component
-import org.springframework.validation.annotation.Validated
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import javax.validation.Valid
 
 @Component
-@Validated
-class GrammarCheckResolver(val service: GrammarCheckService) : GraphQLQueryResolver {
+class GrammarCheckResolver(val service: GrammarCheckService) : Query {
 
-    fun check(@Valid text: GrammarRequest) : GrammarDto {
+    fun check(@Valid text: GrammarRequest) : Mono<GrammarDto> {
         try {
             val results = service.checkGrammar(text.text)
 
-            return if (results.get().isEmpty()) {
-                GrammarDto("", "")
-            } else {
-                results.get()[0]
-            }
+            return results.next()
         } catch (e: IllegalArgumentException) {
             throw GraphQLException(e.message)
         }
     }
 
-    fun log() : List<LogDto> {
+    fun log() : Flux<LogDto> {
         try {
             return service.wordLogList().map { LogDto(it) }
         } catch (e: IllegalArgumentException) {
