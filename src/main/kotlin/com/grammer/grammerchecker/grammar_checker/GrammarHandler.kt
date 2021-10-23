50964d7417b9ab5ce29b491661a3605ef94db0de
lsh <tlgj255@naver.com>
2021-10-23 19:57:21 +0900

    Refactor : grammar 체크 부분 webflux로 변경

--- src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarHandler.kt
+++ src/main/kotlin/com/grammer/grammerchecker/grammar_checker/GrammarHandler.kt
@@ -2,17 +2,15 @@ package com.grammer.grammerchecker.grammar_checker
 
 import com.grammer.grammerchecker.utils.GrammarChecker
 import org.springframework.data.domain.Sort
-import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
-import org.springframework.web.reactive.function.server.ServerResponse.notFound
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
 import reactor.core.publisher.Flux
-import java.nio.charset.Charset
+import java.util.*
 
 @Component
 class GrammarHandler(
@@ -21,10 +19,11 @@ class GrammarHandler(
     ) {
 
     fun checkGrammar(req: ServerRequest): Mono<ServerResponse> = ok()
-        .contentType(MediaType.APPLICATION_JSON)
         .body<Flux<GrammarDto>>(
             checker.checkGrammar(
-                req.queryParam("grammar").orElse("")
+                req.queryParam("grammar")
+                    .filter(Objects::nonNull)
+                    .orElse("")
             )
         )
 
--- src/main/kotlin/com/grammer/grammerchecker/utils/GrammarChecker.kt
+++ src/main/kotlin/com/grammer/grammerchecker/utils/GrammarChecker.kt
@@ -1,14 +1,10 @@
 package com.grammer.grammerchecker.utils
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.grammer.grammerchecker.grammar_checker.GrammarDto
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
-import reactor.core.publisher.Flux
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Component
 class GrammarChecker {
@@ -22,41 +18,33 @@ class GrammarChecker {
     @Value("\${naver.referer}")
     private lateinit var naverReferer: String
 
-    fun checkGrammar(grammar: String = ""): Flux<GrammarDto> {
-        val header = HttpHeaders()
-        header.set("user-gent", naverUserGent)
-        header.set("referer", naverReferer)
+    fun checkGrammar(grammar: String = "") =
+        WebClient.create("$naverUrl$grammar")
+            .get().headers {
+                it.set("user-gent", naverUserGent)
+                it.set("referer", naverReferer)
+            }.retrieve()
+            .bodyToMono(String::class.java)
+            .flatMap {
+                val jsonOpen = it.indexOf("{")
+                val jsonClose = it.lastIndex - 1
+                val json = it.substring(jsonOpen, jsonClose)
+                val expression = RegularExpression()
 
-        val entity = HttpEntity<String>("", header)
+                val errorCount = expression.getInt(
+                    expression.getBetween("errata_count", "origin_html", json)
+                )
+                val errorTextArray = expression.getBetween("origin_html", "html", json).split("</span>")
+                val fixedTextArray = expression.getBetween("html", "notag_html", json).split("</span>")
 
-        val restTemplate = RestTemplate()
-        val respEntity = restTemplate.exchange("$naverUrl$grammar", HttpMethod.GET, entity, String::class.java)
+                val fixedArray = Array(errorCount) { cnt ->
+                    GrammarDto(
+                        errorTextArray[cnt].substring(errorTextArray[cnt].indexOf(">") + 1),
+                        fixedTextArray[cnt].substring(fixedTextArray[cnt].indexOf(">") + 1)
+                    )
+                }
 
-        val objectMapper = ObjectMapper()
-
-        var jsonString = respEntity.body ?: ""
-        val jsonOpen = jsonString.indexOf("{")
-        val jsonClose = jsonString.lastIndex - 1
-
-        jsonString = jsonString.substring(jsonOpen, jsonClose)
-
-        val jsonNode = objectMapper.readTree(jsonString)
-
-        val result = jsonNode.get("message").get("result")
-
-        val errorCount = result.get("errata_count").toString().toInt()
-        val originFullText = result.get("origin_html").toString()
-        val fixedFullText = result.get("html").toString()
-
-        val originTextArray = originFullText.split("</span>")
-        val fixedTextArray = fixedFullText.split("</span>")
-
-        return Flux.fromIterable(
-            MutableList(errorCount) {
-                val errorText = originTextArray[it].substring(originTextArray[it].indexOf(">") + 1)
-                val fixedText = fixedTextArray[it].substring(fixedTextArray[it].indexOf(">") + 1)
-                GrammarDto(errorText, fixedText)
+                Mono.just(fixedArray)
             }
-        )
-    }
+
 }
