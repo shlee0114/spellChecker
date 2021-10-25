139dfe67a46db39fd7aa7adbe982c31c9d1ff894
lsh <tlgj255@naver.com>
2021-10-25 23:17:09 +0900

    Add : errorUtil 추가

--- src/main/kotlin/com/grammer/grammerchecker/utils/ApiUtils.kt
+++ src/main/kotlin/com/grammer/grammerchecker/utils/ApiUtils.kt
@@ -3,5 +3,10 @@ package com.grammer.grammerchecker.utils
 data class ApiUtils<T>(
     val success: Boolean = true,
     val response: T? = null,
-    val error: Map<String, Any>? = null
+    val error: ErrorUtils? = null
+)
+
+data class ErrorUtils(
+    val message: String = "",
+    val status: Int
 )
