71907facf9c4f730968f5064189cee9e3030ec7d
lsh <tlgj255@naver.com>
2021-10-25 23:18:20 +0900

    Add : content type 명시

--- src/main/kotlin/com/grammer/grammerchecker/routers/Router.kt
+++ src/main/kotlin/com/grammer/grammerchecker/routers/Router.kt
@@ -4,6 +4,7 @@ import com.grammer.grammerchecker.handlers.impl.GrammarHandler
 import com.grammer.grammerchecker.handlers.impl.LogHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.RequestPredicates
 import org.springframework.web.reactive.function.server.RouterFunctions.nest
 import org.springframework.web.reactive.function.server.router
@@ -19,6 +20,7 @@ class Router(
     fun routerFunction() = nest(
         RequestPredicates.path("/api"),
         router {
+            contentType(MediaType.APPLICATION_JSON)
             charset("UTF-8")
             listOf(
                 GET("/check", grammarHandler::checkGrammar),
