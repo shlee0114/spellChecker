0eaf6e2735ada4181970337c55cf726f25167fcb
lsh <tlgj255@naver.com>
2021-10-26 21:17:32 +0900

    Add : grammar validation 추가

---
+++ src/main/kotlin/com/grammer/grammerchecker/validator/GrammarValidator.kt
@@ -0,0 +1,28 @@
+package com.grammer.grammerchecker.validator
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ResponseStatusException
+
+
+@Component
+class GrammarValidator {
+    fun validate(text: String) {
+        if(text.isBlank()) {
+            error(
+                ResponseStatusException(
+                    HttpStatus.BAD_REQUEST
+                    , "text is empty"
+                )
+            )
+        }
+        if(text.length > 500) {
+            error(
+                ResponseStatusException(
+                    HttpStatus.BAD_REQUEST
+                    , "text must be less than 500"
+                )
+            )
+        }
+    }
+}
