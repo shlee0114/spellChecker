2ae219f8a13669f2c947ffc07ce0bb47f5228318
lsh <tlgj255@naver.com>
2021-10-26 23:42:24 +0900

    Refactor : 기존 mvc test code -> webflux test code로 변환

--- src/test/kotlin/grammar/GrammarLogTest.kt
+++ src/test/kotlin/grammar/GrammarLogTest.kt
@@ -2,89 +2,93 @@ package grammar
 
 import com.grammer.grammerchecker.GrammerCheckerApplication
 import org.apache.commons.lang3.RandomStringUtils
-import org.hamcrest.Matchers
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
 
-@SpringBootTest(classes = [GrammerCheckerApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+
+@SpringBootTest(classes = [GrammerCheckerApplication::class],webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@AutoConfigureWebTestClient(timeout = "10000")
 class GrammarLogTest {
-
-    private lateinit var mockMvc : MockMvc
-
     @Autowired
-    fun setMockMvc(mockMvc: MockMvc) {
-        this.mockMvc = mockMvc
+    private lateinit var context: ApplicationContext
+
+    private lateinit var webclient: WebTestClient
+
+    @BeforeEach
+    fun setUp() {
+        webclient = WebTestClient.bindToApplicationContext(context).build()
     }
 
     @Test
     @Order(1)
     @DisplayName("등록 성공")
     fun checkSuccess() {
-        val result = mockMvc.perform(
-            MockMvcRequestBuilders.post("/api/log")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content("{\"errorText\":\"되요\",\"fixedText\":\"돼요\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
-        )
-        result.andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.success", Matchers.`is`(true)))
+        webclient.post().uri("/api/log")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(
+                BodyInserters.fromValue("{\"errorText\":\"되요\",\"fixedText\":\"돼요\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
+            ).exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(true)
     }
 
     @Test
     @Order(2)
     @DisplayName("등록 실패(최대 길이 500자 초과)")
     fun checkFailedBecauseMaxLengthExceeded() {
-        var result = mockMvc.perform(
-            MockMvcRequestBuilders.post("/api/log")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content("{\"errorText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedText\":\"돼요\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
-        )
-        result.andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().is4xxClientError)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.success", Matchers.`is`(false)))
+        webclient.post().uri("/api/log")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(
+                BodyInserters.fromValue("{\"errorText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedText\":\"돼요\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
+            ).exchange()
+            .expectStatus().is4xxClientError
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(false)
 
-        result = mockMvc.perform(
-            MockMvcRequestBuilders.post("/api/log")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content("{\"errorText\":\"되요\",\"fixedText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
-        )
-        result.andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().is4xxClientError)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.success", Matchers.`is`(false)))
+        webclient.post().uri("/api/log")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(
+                BodyInserters.fromValue("{\"errorText\":\"돠요\",\"fixedText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
+            ).exchange()
+            .expectStatus().is4xxClientError
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(false)
 
-        result = mockMvc.perform(
-            MockMvcRequestBuilders.post("/api/log")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content("{\"errorText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
-        )
-        result.andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().is4xxClientError)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.success", Matchers.`is`(false)))
+        webclient.post().uri("/api/log")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(
+                BodyInserters.fromValue("{\"errorText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedText\":\"${RandomStringUtils.randomAlphanumeric(501)}\",\"fixedCount\":1,\"ip\":\"123.123.123.123\"}")
+            ).exchange()
+            .expectStatus().is4xxClientError
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(false)
     }
 
     @Test
     @Order(3)
     @DisplayName("로그 조회 성공")
     fun checkSuccessSearchLogList() {
-        val result = mockMvc.perform(MockMvcRequestBuilders.get("/api/log"))
-
-        result.andDo(MockMvcResultHandlers.print())
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.success", Matchers.`is`(true)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.response").isNotEmpty)
+        webclient.get().uri("/api/log")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(true)
+            .jsonPath("$.response").isNotEmpty
     }
 }
--- src/test/kotlin/grammar/GrammarTest.kt
+++ src/test/kotlin/grammar/GrammarTest.kt
@@ -2,85 +2,101 @@ package grammar
 
 import com.grammer.grammerchecker.GrammerCheckerApplication
 import org.apache.commons.lang3.RandomStringUtils
-import org.hamcrest.Matchers
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest(classes = [GrammerCheckerApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
-class GrammarTest{
-
-    private lateinit var mockMvc : MockMvc
-
+@SpringBootTest(
+    classes = [GrammerCheckerApplication::class],
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@DirtiesContext
+@AutoConfigureWebTestClient(timeout = "10000")
+class GrammarTest {
     @Autowired
-    fun setMockMvc(mockMvc: MockMvc){
-        this.mockMvc = mockMvc
+    private lateinit var context: ApplicationContext
+
+    private lateinit var webclient: WebTestClient
+
+    @BeforeEach
+    fun setUp() {
+        webclient = WebTestClient.bindToApplicationContext(context).build()
     }
 
     @Test
     @Order(1)
     @DisplayName("검색 성공")
-    fun checkSuccess(){
-        val result = mockMvc.perform(
-            get("/api/check")
-                .param("grammar", "이거이렇게 안돼나요")
-        )
-        result.andDo(print())
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("$.success", Matchers.`is`(true)))
-            .andExpect(jsonPath("$.response").isNotEmpty)
+    fun checkSuccess() {
+        webclient.get().uri { builder ->
+            builder
+                .path("/api/check")
+                .queryParam("grammar", "이거이렇게 안돼나요")
+                .build()
+        }
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(true)
+            .jsonPath("$.response").isNotEmpty
     }
+
     @Test
     @Order(2)
     @DisplayName("검색 성공(수정 없음)")
-    fun checkSuccessReturnIsNull(){
-        val result = mockMvc.perform(
-            get("/api/check")
-                .param("grammar", "돼요")
-        )
-        result.andDo(print())
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("$.success", Matchers.`is`(true)))
-            .andExpect(jsonPath("$.response").isEmpty)
+    fun checkSuccessReturnIsNull() {
+        webclient.get().uri { builder ->
+            builder
+                .path("/api/check")
+                .queryParam("grammar", "돼요")
+                .build()
+        }
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(true)
+            .jsonPath("$.response").isEmpty
     }
 
     @Test
     @Order(3)
     @DisplayName("검색 실패(최대 길이 500자 초과)")
-    fun checkFailedBecauseMaxLengthExceeded(){
-        val result = mockMvc.perform(
-            get("/api/check")
-                .param("grammar", RandomStringUtils.randomAlphanumeric(501))
-        )
-        result.andDo(print())
-            .andExpect(status().is5xxServerError)
-            .andExpect(jsonPath("$.success", Matchers.`is`(false)))
+    fun checkFailedBecauseMaxLengthExceeded() {
+        webclient.get().uri { builder ->
+            builder
+                .path("/api/check")
+                .queryParam("grammar", RandomStringUtils.randomAlphanumeric(501))
+                .build()
+        }
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().is4xxClientError
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(false)
+            .jsonPath("$.response").isEmpty
     }
+
     @Test
     @Order(4)
     @DisplayName("검색 실패(빈 텍스트 전달)")
-    fun checkFailedBecauseEmptyText(){
-        val result = mockMvc.perform(
-            get("/api/check")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .param("grammar", "")
-        )
-        result.andDo(print())
-            .andExpect(status().is5xxServerError)
-            .andExpect(jsonPath("$.success", Matchers.`is`(false)))
+    fun checkFailedBecauseEmptyText() {
+        webclient.get().uri { builder ->
+            builder
+                .path("/api/check")
+                .queryParam("grammar", "")
+                .build()
+        }
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().is4xxClientError
+            .expectBody()
+            .jsonPath("$.success").isEqualTo(false)
+            .jsonPath("$.response").isEmpty
     }
-
-
-
 }
