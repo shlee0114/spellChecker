2b3b805c26870e687c3d7bb383668c49b1bbc6d8
lsh <tlgj255@naver.com>
2021-10-26 21:17:42 +0900

    Feat : grammar validation 적용

--- src/main/kotlin/com/grammer/grammerchecker/handlers/impl/GrammarHandler.kt
+++ src/main/kotlin/com/grammer/grammerchecker/handlers/impl/GrammarHandler.kt
@@ -2,6 +2,8 @@ package com.grammer.grammerchecker.handlers.impl
 
 import com.grammer.grammerchecker.utils.ApiUtils
 import com.grammer.grammerchecker.utils.GrammarChecker
+import com.grammer.grammerchecker.validator.GrammarValidator
+import com.grammer.grammerchecker.validator.LogValidator
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -9,18 +11,24 @@ import reactor.core.publisher.Mono
 
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
+import reactor.core.publisher.Flux
 import java.util.*
 
 @Component
-class GrammarHandler(private val checker: GrammarChecker) {
+class GrammarHandler(
+    private val checker: GrammarChecker,
+    private val validator: GrammarValidator
+) {
 
     fun checkGrammar(req: ServerRequest): Mono<ServerResponse> = ok()
         .body(
-            checker.checkGrammar(
+            Flux.just(
                 req.queryParam("grammar")
-                    .filter(Objects::nonNull)
                     .orElse("")
-            ).collectList()
+            ).flatMap {
+                validator.validate(it)
+                checker.checkGrammar(it)
+            }.collectList()
                 .flatMap {
                     Mono.just(
                         ApiUtils(response = it)
