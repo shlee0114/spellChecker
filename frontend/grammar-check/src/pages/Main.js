828b2ee2fc0722da8c0406e8f1752df3f308983a
lsh <tlgj255@naver.com>
2021-10-27 22:34:36 +0900

    Add : 로그 보기 추가

--- frontend/grammar-check/src/pages/Main.js
+++ frontend/grammar-check/src/pages/Main.js
@@ -9,10 +9,13 @@ require('dotenv').config()
 export default function Main (){
 
     const [ fixedText, setFixedText ] = useState('');
+    const [ wordHistoryText, setWordHistory ] = useState('');
+    const [ sentenceHistoryText, setSentenceHistory ] = useState('');
     const [ textCount, setCount ] = useState(0);
     const [ sendText, setText ] = useState('');
     const [ resultist, setResult ] = useState([]);
     const [ip, setIP] = useState('');
+    const serverIp = 'http://zifori.me/api/'
 
     var isCtrl = false
     var serverSendTimer = null
@@ -39,7 +42,7 @@ export default function Main (){
             if(resultist.length !== 0){
                 const beforeText = inputArea.value
 
-                resultist.map((result) => {
+                resultist.map(result => {
                     inputArea.value = inputArea.value.replaceAll(result.errorText, result.fixedText)
                     url.mutate({
                         mutation:grammar.LOG_INSERT(result.errorText, result.fixedText, ip)
@@ -48,7 +51,8 @@ export default function Main (){
 
                 const afterText = inputArea.value
 
-                axios.post(`${process.env.SERVER_IP}/api/log`, {errorText: beforeText, fixedText: afterText, fixedCount: resultist.length, ip: ip})
+                axios.post(`${serverIp}log`, 
+                {errorText: beforeText, fixedText: afterText, fixedCount: resultist.length, ip: ip})
             }else{
                 inputArea.value = inputArea.value.replace(new RegExp(sendText+ '$'), fixedText)
                 
@@ -89,13 +93,13 @@ export default function Main (){
 
         setResult([])
         if(totalYn){
-            axios.get(`${process.env.SERVER_IP}/api/check?grammar=${inputArea.value}`)
+            axios.get(`${serverIp}check?grammar=${inputArea.value}`)
             .then(result => {
                 const list = result.data.response
                 const resultist = []
                 var fix = ''
 
-                list.map((info) => (
+                list.map(info => (
                     fix += `${info.errorText} -> ${info.fixedText}\n`,
                     resultist.push({
                         errorText : info.errorText,
@@ -105,20 +109,56 @@ export default function Main (){
 
                 setFixedText(fix)
                 setResult(resultist)
+            }).catch(res => {
+                console.log(res)
             })
         }else{
             url.query({
                 query : grammar.GRAMMAR_CHECK(sendText)
             }).then(result => {
                 if(!(result.errors??false)){
-                    if(result.data.check.fixedText != null){
+                    if((result.data.check.fixedText??"") !== ""){
                         setFixedText(result.data.check.fixedText)
                     }else{
                         setFixedText(sendText)
                     }
                 }
+            }).catch(res => {
+                console.log(res)
             })
         }
+    }
+
+    const searchWordHistory = () => {
+        url.query({
+            query : grammar.SEARCH_LOG()
+        }).then(result => {
+
+            const list = result.data.log
+            var log = ''
+
+            list.map(info => (
+                log += `${info.error} -> ${info.fixed}\ntime : ${info.fixedTime}\n\n`
+            ))
+            setWordHistory(log)
+        }).catch(res => {
+            console.log(res)
+        })
+    }
+
+    const searchSentenceHistory = () => {
+        axios.get(`${serverIp}log`)
+        .then(result => {
+            const list = result.data.response
+            var log = ''
+
+            list.map(info => (
+                log += `${info.error} -> ${info.fixed}\ncount : ${info.count}\ntime : ${info.fixedTime}\n\n`
+            ))
+            setSentenceHistory(log)
+        }).catch(res => {
+            console.log(res)
+        })
     }
 
     return (
@@ -131,20 +171,18 @@ export default function Main (){
                     <textarea className="inputArea" id="input" cols="30" rows="5"
                     onKeyDown={(e)=>{inputTextKeyDown(e)}}
                     onKeyUp={(e) => {inputTextKeyUp(e)}}>
-
                     </textarea>
                     <textarea className="resultArea" readOnly value={fixedText}>
-                        {fixedText}
                     </textarea>
                 </div>
             </section>
             <section className="history">
-                <button className="searchWordHistory" >낱말 수정 기록</button>
-                <button className="searchSentenceHistory" >문장 수정 기록</button>
+                <button className="searchWordHistory" onClick={() => searchWordHistory()}>낱말 수정 기록</button>
+                <button className="searchSentenceHistory" onClick={() => searchSentenceHistory()}>문장 수정 기록</button>
                 <div className="historyTextarea">
-                    <textarea className="wordHistory" id="input" cols="30" rows="5">
+                    <textarea className="wordHistory" readOnly value={wordHistoryText}>
                     </textarea>
-                    <textarea className="sentenceHistory" readOnly value={fixedText}>
+                    <textarea className="sentenceHistory" readOnly value={sentenceHistoryText}>
                     </textarea>
                 </div>
             </section>
