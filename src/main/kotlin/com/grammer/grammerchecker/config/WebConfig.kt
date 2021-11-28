bd5d2a7530ec4e9ca5c05da5726c405cd9254b7d
lsh <tlgj255@naver.com>
2021-11-28 21:24:39 +0900

    Refactor : router webConfig 로 이동

--- src/main/kotlin/com/grammer/grammerchecker/config/WebConfig.kt
+++ src/main/kotlin/com/grammer/grammerchecker/config/WebConfig.kt
@@ -1,21 +1,45 @@
 package com.grammer.grammerchecker.config
 
+import com.grammer.grammerchecker.handlers.GrammarHandler
+import com.grammer.grammerchecker.handlers.LogHandler
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.router
 
 
 @EnableWebFlux
 @Configuration
-class WebConfig : WebFluxConfigurer {
+class WebConfig(
+    private val grammarHandler: GrammarHandler,
+    private val logHandler: LogHandler
+    ) : WebFluxConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry
             .addMapping("/**")
             .allowedHeaders("*")
-            .allowedOrigins("http://zifori.me:3000")
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods("*")
             .maxAge(1800)
             .allowCredentials(false)
     }
+
+    @Bean
+    fun routerFunction() = RouterFunctions.nest(
+        RequestPredicates.path("/api"),
+        router {
+            contentType(MediaType.APPLICATION_JSON)
+            charset("UTF-8")
+            listOf(
+                GET("/check", grammarHandler::checkGrammar),
+                GET("/log", logHandler::logList),
+                POST("/log", logHandler::insertLog)
+            )
+        }
+    )
 }
