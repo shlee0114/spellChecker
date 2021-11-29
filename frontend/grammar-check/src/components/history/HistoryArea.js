0aad359b3bff2643a0bab93fa509a195706836e1
lsh <tlgj255@naver.com>
2021-11-29 22:22:47 +0900

    Add : components들 추가

---
+++ frontend/grammar-check/src/components/history/HistoryArea.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HistorySection = styled.section`
+margin-top: 30px;
+`
+
+
+const HistoryArea = ({wordText, sentenceText}) => {
+    <HistorySection>
+        
+    </HistorySection>
+}
