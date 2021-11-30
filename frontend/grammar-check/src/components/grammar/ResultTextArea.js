2e9caddd3f90be6c31cd6434270b1afbb8ac14b4
lsh <tlgj255@naver.com>
2021-11-30 22:50:19 +0900

    Add : ResultTextArea

--- frontend/grammar-check/src/components/grammar/ResultTextArea.js
+++ frontend/grammar-check/src/components/grammar/ResultTextArea.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import styled from "styled-components";
+import { CloseButton } from '../common/CloseButton';
 
 const Area = styled.article`
-  width: 30%;
-  height: 100%;
+    width: 24%;
+    height: 100%;
+    margin-left: 1px;
 `;
+
+const CorverArea = styled.div`
+    width: 100%;
+    height: 100%;
+    position:absolute;
+    background:#EFEFEC
+`
+const FixedArea = styled.div`
+    width: 100%;
+    height: 100%;
+    background:#DDDDDD;
+`
+const TextArea = styled.textarea`
+  margin: 10%;
+  width: 80%;
+  height: 80%;
+  font-size: 1rem;
+  border: 1px solid rgba(95, 92, 92, 0.47);
+  outline: 1px solid rgba(95, 92, 92, 0.47);
+  resize: none;
+  font-family: 'noto sans KR';
+  
+`;
+
 export const ResultTextArea = () => {
     return (
         <Area>
-
+            <CorverArea/>
+            <FixedArea>
+                <CloseButton></CloseButton>
+                <TextArea readOnly></TextArea>
+            </FixedArea>
         </Area>
     )
 } 
