e727f03455083381684b69644983831b0a22ffe2
lsh <tlgj255@naver.com>
2021-11-30 21:25:13 +0900

    Add : GrammarArea

--- frontend/grammar-check/src/components/grammar/GrammarArea.js
+++ frontend/grammar-check/src/components/grammar/GrammarArea.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import {InputTextArea} from './InputTextArea'
+import {ResultTextArea} from './ResultTextArea'
 
-const InputArea = ({children}) => {
-    
-}
+const Area = styled.section`
+    width: 70%;
+    height: 60vh;
+    margin-left: 15%;
+    margin-top: 10%;
+    background: #F7F7F7;
+    box-sizing: border-box;
+    border: 1px solid rgba(95, 92, 92, 0.47);
+`
+
+export const GrammarArea = () => {
+    return (
+        <Area>
+        <InputTextArea/>
+        <ResultTextArea/>
+        </Area>
+    )
+} 
