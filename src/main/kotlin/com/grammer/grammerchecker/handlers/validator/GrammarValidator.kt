491824f75e501a0463ba572ad52187fa3a78f677
lsh <tlgj255@naver.com>
2021-12-16 22:57:23 +0900

    Refactor : CommonValidator 제거
    errors를 수집할 이유가 없으며, 불필요한 추가 작업들이라 생각하여 제거

--- src/main/kotlin/com/grammer/grammerchecker/handlers/validator/GrammarValidator.kt
+++ src/main/kotlin/com/grammer/grammerchecker/handlers/validator/GrammarValidator.kt
@@ -1,29 +1,20 @@
 package com.grammer.grammerchecker.handlers.validator
 
-import com.grammer.grammerchecker.utils.CommonValidator
 import com.grammer.grammerchecker.utils.ValidationText
 import org.springframework.stereotype.Component
-import org.springframework.validation.Errors
-
 
 @Component
-class GrammarValidator : CommonValidator() {
-    override fun supports(clazz: Class<*>) =
-        String::class.java.isAssignableFrom(clazz)
-
-    override fun validate(target: Any, errors: Errors) {
-        val text = target as String
-
-        if (text.isBlank()) {
+class GrammarValidator {
+    fun validationCheck(target: String) {
+        if (target.isBlank()) {
             error(
                 ValidationText.invalidValue("grammar")
             )
         }
-        if (text.length > 500) {
+        if (target.length > 500) {
             error(
                 ValidationText.sizeLess("grammar", 500)
             )
         }
-
     }
 }
--- src/main/kotlin/com/grammer/grammerchecker/handlers/validator/LogValidator.kt
+++ src/main/kotlin/com/grammer/grammerchecker/handlers/validator/LogValidator.kt
@@ -1,41 +1,31 @@
 package com.grammer.grammerchecker.handlers.validator
 
 import com.grammer.grammerchecker.handlers.model.dto.LogRequest
-import com.grammer.grammerchecker.utils.CommonValidator
 import com.grammer.grammerchecker.utils.ValidationText
 import org.springframework.stereotype.Component
-import org.springframework.validation.Errors
-import org.springframework.validation.ValidationUtils
 
 @Component
-class LogValidator : CommonValidator() {
-    override fun supports(clazz: Class<*>) =
-        LogRequest::class.java.isAssignableFrom(clazz)
+class LogValidator {
+    fun validationCheck(target: LogRequest) {
+        if(target.errorText.isBlank()) {
+            error(
+                ValidationText.invalidValue("errorText")
+            )
+        }
+        if(target.fixedText.isBlank()) {
+            error(
+                ValidationText.invalidValue("fixedText")
+            )
+        }
 
-    override fun validate(target: Any, errors: Errors) {
-        ValidationUtils.rejectIfEmptyOrWhitespace(
-            errors,
-            "errorText",
-            ValidationText.invalidValue("errorText")
-        )
-        ValidationUtils.rejectIfEmptyOrWhitespace(
-            errors,
-            "fixedText",
-            ValidationText.invalidValue("fixedText")
-        )
-
-        val request = target as LogRequest
-
-        if (request.errorText.trim().length > 500) {
-            errors.rejectValue(
-                "errorText",
+        if (target.errorText.trim().length > 500) {
+            error(
                 ValidationText.sizeLess("errorText", 500)
             )
         }
 
-        if (request.fixedText.trim().length > 500) {
-            errors.rejectValue(
-                "fixedText",
+        if (target.fixedText.trim().length > 500) {
+            error(
                 ValidationText.sizeLess("fixedText", 500)
             )
         }
