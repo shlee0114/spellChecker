4fe5705f8c6489aed47d1598114ee156b16b4c55
lsh <tlgj255@naver.com>
2021-12-18 17:33:00 +0900

    Refactor : frontend 분리

--- frontend/grammar-check/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import './static/font/font.css'
-
-ReactDOM.render(
-  <App/>
-,document.getElementById('root'));
--- frontend/grammar-check/src/static/setting.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export const serverIp = `${process.env.REACT_APP_SERVER_IP}/api/`
-export const graphqlServerIp = `${process.env.REACT_APP_SERVER_IP}/graphql`
